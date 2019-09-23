a694b281d98f5dc16d2dc8c35a88d5220832cf6f
vtymashkov <Box8lister>
2019-09-23 16:04:19 +0300

    Updated

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     static class A {
@@ -5,12 +7,16 @@ public class Main {
             System.out.println("A " + s);
         }
 
-        //Necessary constructor
+        //Necessary constructor!
         public A() {
         }
 
         public void doSmth() {
             System.out.println("A");
+        }
+
+        public void doSmthOtherFromA() {
+            System.out.println("Do something other from A");
         }
     }
 
@@ -24,28 +30,70 @@ public class Main {
         public void doSmth() {
             System.out.println("B");
         }
+
+        public void doSmthOtherFromB() {
+            System.out.println("Do something other from B");
+        }
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Inheritance Tests - 1!");
+
+        System.out.println("\nInheritance Tests - 1!");
         A a = new B("b"); // B b
         a.doSmth();         // B
 
 
-        System.out.println("Inheritance Tests - 2!");
-        A a1 = new A ("b"); // A b
-        a1.doSmth();          // A
+        System.out.println("\nInheritance Tests - 2!");
+        A aaa = new A("a"); // A a
+        aaa.doSmth();          // A
 
         //Impossible to cast A to B
-//        System.out.println("Inheritance Tests - 3!");
+//        System.out.println("\nInheritance Tests - 3!");
 //        B a2 = (B) new A ("b");
 //        a2.doSmth();
 
-        System.out.println("Inheritance Tests - 4!");
-        B a3 = new B ("c"); // B c
-        a3.doSmth();          // B
+        System.out.println("\nInheritance Tests - 4!");
+        B b = new B("b"); // B b
+        b.doSmth();          // B
 
+
+        ////////////////////////////////////////////////////////
+        A b1 = new B();
+        b.doSmthOtherFromA();           //We can use methods from class A
+        //b1.doSmthOtherFromB();        //doSmthOtherFromB() is not accessible for the object of type A
+        ((B) b1).doSmthOtherFromB();  //but it is possible to cast it to type B
+
+
+
+
+////////////////////////////////////////////////////////
+        /* ANOTHER CHECK */
+        System.out.println("\n************* ANOTHER CHECK!*************\n");
+
+
+        System.out.println("\n**** A aa = new A();");
+        A aa = new A();
+        aa.doSmth();
+        aa.doSmthOtherFromA();
+        //aa.doSmthOtherFromB();
+
+        System.out.println("\n**** A ab = new B();");
+        A ab = new B();
+        ab.doSmth();
+        ab.doSmthOtherFromA();
+        //ab.doSmthOtherFromB();
+
+
+        System.out.println("\n**** B bb = new B();");
+        B bb = new B();
+        bb.doSmth();
+        bb.doSmthOtherFromA();
+        bb.doSmthOtherFromB();
+
+        //B ba = new A();     //Is not possible
+/////////////////////////////////////////////////////////
     }
+
 }
 
